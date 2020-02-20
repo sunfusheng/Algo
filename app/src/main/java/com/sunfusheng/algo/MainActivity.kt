0953fbdb8631ddca013a89567ede6123a1b1d49e
sunfusheng <sfsheng0322@gmail.com>
2020-02-20 13:16:21 +0800

    init

---
+++ app/src/main/java/com/sunfusheng/algo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sunfusheng.algo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
