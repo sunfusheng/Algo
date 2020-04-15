5f6e9b87704267459a5bede151daaa05ca3644ac
sunfusheng <sfsheng0322@gmail.com>
2020-04-15 15:04:43 +0800

    判断两个无环链表是否相交，相交则返回第一个相交节点

--- module-algo/src/main/java/com/sunfusheng/algo/Algo/LinkedList/IntersectNode.java
+++ module-algo/src/main/java/com/sunfusheng/algo/Algo/LinkedList/IntersectNode.java
@@ -27,24 +27,24 @@ import com.sunfusheng.algo.common.model.Node;
 public class IntersectNode {
 
     /**
-     * 判断一个链表是否有环，如果有，则返回第一个进入环的节点
+     * 问题一：判断一个链表是否有环，如果有，则返回第一个进入环的节点，没有则返回null。
      *
      * @param head
      * @return
      */
     public static Node getLoopNode(Node head) {
-        if (head == null) {
+        if (head == null || head.next == null || head.next.next == null) {
             return null;
         }
 
-        Node fast = head;
-        Node slow = head;
-        while (fast != null && fast.next != null) {
+        Node fast = head.next.next;
+        Node slow = head.next;
+        while (fast != slow) {
+            if (fast.next == null || fast.next.next == null) {
+                return null;
+            }
             fast = fast.next.next;
             slow = slow.next;
-            if (fast == slow) {
-                break;
-            }
         }
 
         fast = head;
@@ -53,5 +53,105 @@ public class IntersectNode {
             slow = slow.next;
         }
         return fast;
+    }
+
+    /**
+     * 问题二：判断两个无环链表是否相交，相交则返回第一个相交节点，不相交则返回null。
+     *
+     * @param head1
+     * @param head2
+     * @return
+     */
+    public static Node getNoLoopNode(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            cur1 = cur1.next;
+            n++;
+        }
+        while (cur2.next != null) {
+            cur2 = cur2.next;
+            n--;
+        }
+
+        // cur1 和 cur2 为null，都是最后一个节点
+        if (cur1 != cur2) {
+
+            return null;
+        }
+
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+        n = Math.abs(n);
+        while (n != 0) {
+            cur1 = cur1.next;
+            n--;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    public static void main(String[] args) {
+        /*
+         * 构造一个有环的单链表，结构如下：
+         *
+         * ● 1 -> 2 -> 3 -> 4
+         *             ↑    ↓
+         *             6 <- 5
+         */
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node3;
+
+        Node firstLoopNode = getLoopNode(node1);
+        System.out.println("判断一个链表是否有环，如果有，则返回第一个进入环的节点: " +
+                (firstLoopNode != null ? firstLoopNode.value : firstLoopNode));
+
+        /*
+         * 构造两个无环且相交的单链表，结构如下：
+         *
+         * ● 1 -> 2 -> 3 -> 4
+         *                  ↓
+         *      ● 3 -> 4 -> 5 -> 6 -> 7 ->8
+         */
+        Node node11 = new Node(1);
+        Node node12 = new Node(2);
+        Node node13 = new Node(3);
+        Node node14 = new Node(4);
+        Node node15 = new Node(5);
+        Node node16 = new Node(6);
+        Node node17 = new Node(7);
+        Node node18 = new Node(8);
+        node11.next = node12;
+        node12.next = node13;
+        node13.next = node14;
+        node14.next = node15;
+        node15.next = node16;
+        node16.next = node17;
+        node17.next = node18;
+
+        Node node23 = new Node(3);
+        Node node24 = new Node(4);
+        node23.next = node24;
+        node24.next = node15;
+        firstLoopNode = getNoLoopNode(node11, node23);
+        System.out.println("判断两个无环链表是否相交，相交则返回第一个相交节点: " +
+                (firstLoopNode != null ? firstLoopNode.value : firstLoopNode));
     }
 }
