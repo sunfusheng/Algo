463705bdddbeac26518cd1c2348869b6069ad3e8
sunfusheng <sunfusheng1989@126.com>
2022-05-22 21:50:19 +0800

    42.接雨水

--- module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/Array/HolderMaxWater.java
+++ module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/Array/HolderMaxWater.java
@@ -6,15 +6,20 @@ import com.sunfusheng.algo.common.util.AlgoUtil;
  * 【题目】
  * 11.盛最多水的容器
  * <p>
- * 给定一个长度为 n 的整数数组 height。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i])。
+ * 给定一个长度为 n 的整数数组 height。有 n 条垂线，第 i 条线的两个端点是 (i, 0)和 (i, height[i])。
  * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
  * 返回容器可以储存的最大水量。
  * <p>
- * 说明：你不能倾斜容器，且 n 的值至少为 2。
+ * 说明：你不能倾斜容器。
  * <p>
- * 示例：
+ * 示例 1：
  * 输入：[1,8,6,2,5,4,8,3,7]
  * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为49。
+ * <p>
+ * 示例 2：
+ * 输入：height = [1,1]
+ * 输出：1
  *
  * @author sunfusheng
  * @since 2020/4/26
@@ -27,9 +32,6 @@ public class HolderMaxWater {
      * 复杂度分析
      * 时间复杂度：O(N)
      * 空间复杂度：O(1)
-     *
-     * @param height
-     * @return
      */
     public static int solution(int[] height) {
         if (height == null || height.length < 2) {
