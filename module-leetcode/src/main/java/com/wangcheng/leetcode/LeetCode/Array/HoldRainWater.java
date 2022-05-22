6098d3e7ba368c795a3892110f10f8d910b51917
sunfusheng <sunfusheng1989@126.com>
2022-05-22 21:45:55 +0800

    42.接雨水

--- module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/Array/HoldRainWater.java
+++ module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/Array/HoldRainWater.java
@@ -8,12 +8,16 @@ import java.util.Stack;
  * 【题目】
  * 42.接雨水
  * <p>
- * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
- * 如果不理解题意，请参考官方示意图 
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
  * <p>
- * 示例:
- * 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
- * 输出: 6
+ * 示例 1：
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ * <p>
+ * 示例 2：
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
  *
  * @author sunfusheng
  * @since 2020/4/26
@@ -26,9 +30,6 @@ public class HoldRainWater {
      * 复杂度分析
      * 时间复杂度：O(N)
      * 空间复杂度：O(N)
-     *
-     * @param height
-     * @return
      */
     public static int solution1(int[] height) {
         if (height == null || height.length < 2) {
@@ -63,9 +64,6 @@ public class HoldRainWater {
      * 复杂度分析
      * 时间复杂度：O(N)
      * 空间复杂度：O(1)
-     *
-     * @param height
-     * @return
      */
     public static int solution2(int[] height) {
         if (height == null || height.length < 2) {
