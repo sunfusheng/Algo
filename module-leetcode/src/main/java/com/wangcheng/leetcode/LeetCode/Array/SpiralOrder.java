1e65281648898a1368064a56b96d7880cd100e1a
sunfusheng <sunfusheng1989@126.com>
2022-06-01 09:14:19 +0800

    54.螺旋矩阵

--- module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/Array/SpiralOrder.java
+++ module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/Array/SpiralOrder.java
@@ -38,9 +38,6 @@ public class SpiralOrder {
      * <p>
      * 时间复杂度：O(n)
      * 空间复杂度：O(n)
-     *
-     * @param matrix
-     * @return
      */
     public static List<Integer> spiralOrder(int[][] matrix) {
         if (matrix == null || matrix.length == 0) {
@@ -48,24 +45,24 @@ public class SpiralOrder {
         }
 
         List<Integer> res = new ArrayList<>();
-        int r1 = 0;
-        int r2 = matrix[0].length - 1;
-        int c1 = 0;
-        int c2 = matrix.length - 1;
+        int r1 = 0; // 左上角的行
+        int c1 = 0; // 左上角的列
+        int r2 = matrix.length - 1; // 右下角的行
+        int c2 = matrix[0].length - 1; // 右下角的列
         while (r1 <= r2 && c1 <= c2) {
-            for (int i = r1; i <= r2; i++)
-                res.add(matrix[c1][i]);
-            for (int i = c1 + 1; i <= c2; i++)
-                res.add(matrix[i][r2]);
+            for (int i = c1; i <= c2; i++)
+                res.add(matrix[r1][i]);
+            for (int i = r1 + 1; i <= r2; i++)
+                res.add(matrix[i][c2]);
             if (r1 < r2 && c1 < c2) {
-                for (int i = r2 - 1; i >= r1; i--)
-                    res.add(matrix[c2][i]);
-                for (int i = c2 - 1; i > c1; i--)
-                    res.add(matrix[i][r1]);
+                for (int i = c2 - 1; i >= c1; i--)
+                    res.add(matrix[r2][i]);
+                for (int i = r2 - 1; i > r1; i--)
+                    res.add(matrix[i][c1]);
             }
             r1++;
-            r2--;
             c1++;
+            r2--;
             c2--;
         }
         return res;
@@ -82,9 +79,9 @@ public class SpiralOrder {
                 {5, 6, 7, 8},
                 {9, 10, 11, 12}
         };
-        System.out.print("方法一输出1：");
+        System.out.print("输出1：");
         AlgoUtil.printlnList(spiralOrder(matrix));
-        System.out.print("方法一输出2：");
+        System.out.print("输出2：");
         AlgoUtil.printlnList(spiralOrder(matrix2));
     }
 }
