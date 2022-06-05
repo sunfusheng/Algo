cc8d1fca5df76b5577dc12097ce3e223d69f57f4
sunfusheng <sunfusheng1989@126.com>
2022-06-05 09:58:12 +0800

    146.LRU 缓存

--- module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/LinkedList/LRUCache.java
+++ module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/LinkedList/LRUCache.java
@@ -1,40 +1,44 @@
 package com.wangcheng.leetcode.LeetCode.LinkedList;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * 【题目】
- * 146.LRU缓存机制
+ * 146.LRU 缓存
+ * 请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
+ * 实现 LRUCache 类：
+ * LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；
+ * 如果不存在，则向缓存中插入该组 key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+ * 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
  * <p>
- * 运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制。
- * 它应该支持以下操作： 获取数据 get 和 写入数据 put 。
+ * 示例：
+ * 输入
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * 输出
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
  * <p>
- * 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
- * 写入数据 put(key, value) - 如果密钥已经存在，则变更其数据值；如果密钥不存在，
- * 则插入该组「密钥/数据值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，
- * 从而为新的数据值留出空间。
- * <p>
- * 进阶:
- * 你是否可以在 O(1) 时间复杂度内完成这两种操作？
- * <p>
- * 示例:
- * LRUCache cache = new LRUCache( 2 );
- * cache.put(1,1);
- * cache.put(2,2);
- * cache.get(1);       // 返回  1
- * cache.put(3,3);     // 该操作会使得密钥 2 作废
- * cache.get(2);       // 返回 -1 (未找到)
- * cache.put(4,4);     // 该操作会使得密钥 1 作废
- * cache.get(1);       // 返回 -1 (未找到)
- * cache.get(3);       // 返回  3
- * cache.get(4);       // 返回  4
+ * 解释
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // 缓存是 {1=1}
+ * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+ * lRUCache.get(1);    // 返回 1
+ * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+ * lRUCache.get(2);    // 返回 -1 (未找到)
+ * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+ * lRUCache.get(1);    // 返回 -1 (未找到)
+ * lRUCache.get(3);    // 返回 3
+ * lRUCache.get(4);    // 返回 4
  *
  * @author sunfusheng
  * @since 2020/4/26
  */
 public class LRUCache extends LinkedHashMap<Integer, Integer> {
 
-    private int capacity;
+    private final int capacity;
 
     public LRUCache(int initialCapacity) {
         super(initialCapacity, 0.75f, true);
@@ -53,7 +57,7 @@ public class LRUCache extends LinkedHashMap<Integer, Integer> {
     }
 
     @Override
-    protected boolean removeEldestEntry(Entry<Integer, Integer> eldest) {
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
         return size() > capacity;
     }
 
