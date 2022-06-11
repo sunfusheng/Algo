c9537dc58d0b35c6638cc816cc3dc68367b9c838
sunfusheng <sunfusheng1989@126.com>
2022-06-11 18:33:15 +0800

    58.最后一个单词的长度

--- module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/String/LengthOfLastWord.java
+++ module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/String/LengthOfLastWord.java
@@ -3,47 +3,48 @@ package com.wangcheng.leetcode.LeetCode.String;
 import com.sunfusheng.algo.common.util.LeetCodeUtil;
 
 /**
- *【题目】
+ * 【题目】
  * 58.最后一个单词的长度
- * 给定一个仅包含大小写字母和空格' '的字符串s，
- * 返回其最后一个单词的长度。如果字符串从左向右滚动显示，
- * 那么最后一个单词就是最后出现的单词。
+ * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个单词的长度。
  * <p>
- * 如果不存在最后一个单词，请返回 0 。
- * 说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
  * <p>
- *【示例】
- * 输入: "Hello World"
- * 输出: 5
+ * 示例 1：
+ * 输入：s = "Hello World"
+ * 输出：5
+ * 解释：最后一个单词是“World”，长度为5。
+ * <p>
+ * 示例 2：
+ * 输入：s = "   fly me   to   the moon  "
+ * 输出：4
+ * 解释：最后一个单词是“moon”，长度为4。
  *
  * @author liwangcheng
  * @date 2020/3/24.
  */
 public class LengthOfLastWord {
 
-    private static final char SPACE = ' ';
-
-    public static int solution(String str) {
-        if (null == str || str.isEmpty()) {
+    public static int solution(String s) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
-        int end = str.length() - 1;
-        while(end >= 0 && str.charAt(end) == SPACE) {
+        int end = s.length() - 1;
+        while (end >= 0 && s.charAt(end) == ' ') {
             end--;
         }
-        if(end < 0) {
+        if (end < 0) {
             return 0;
         }
         int start = end;
-        while(start >= 0 && str.charAt(start) != SPACE) {
+        while (start >= 0 && s.charAt(start) != ' ') {
             start--;
         }
         return end - start;
     }
 
     public static void main(String[] args) {
-        LeetCodeUtil.logln("solution(hello world) = " + LengthOfLastWord.solution("hello world"));
-        LeetCodeUtil.logln("solution(hello ) = " + LengthOfLastWord.solution("hello "));
-        LeetCodeUtil.logln("solution(hello q) = " + LengthOfLastWord.solution("hello o"));
+        LeetCodeUtil.logln("solution(hello world) = " + solution("hello world"));
+        LeetCodeUtil.logln("solution(hello ) = " + solution("hello "));
+        LeetCodeUtil.logln("solution(hello q) = " + solution("hello o"));
     }
 }
