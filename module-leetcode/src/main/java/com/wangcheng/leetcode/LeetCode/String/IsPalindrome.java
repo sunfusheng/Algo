6cafe5cf7040681733183868aee36d21b903b28d
sunfusheng <sunfusheng1989@126.com>
2022-06-12 08:21:37 +0800

    125.验证回文串

--- module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/String/IsPalindrome.java
+++ module-leetcode/src/main/java/com/wangcheng/leetcode/LeetCode/String/IsPalindrome.java
@@ -7,56 +7,50 @@ import com.sunfusheng.algo.common.util.LeetCodeUtil;
  * 125.验证回文串
  * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
  * <p>
- * 说明：本题中，将空字符串定义为有效的回文串。
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
  * <p>
- * 【示例】
  * 示例 1:
  * 输入: "A man, a plan, a canal: Panama"
  * 输出: true
+ * 解释："amanaplanacanalpanama" 是回文串
  * <p>
  * 示例 2:
  * 输入: "race a car"
  * 输出: false
+ * 解释："raceacar" 不是回文串
+ * <p>
+ * 提示：
+ * 1 <= s.length <= 2 * 105
+ * 字符串 s 由 ASCII 字符组成
  *
  * @author liwangcheng
  * @date 2020/4/6.
  */
 public class IsPalindrome {
 
-    public static boolean solution(String str) {
-        if (null == str || str.isEmpty()) {
+    public static boolean solution(String s) {
+        if (s == null || s.isEmpty()) {
             return true;
         }
-        str = str.toLowerCase();
+        s = s.toLowerCase();
         int left = 0;
-        int right = str.length() - 1;
+        int right = s.length() - 1;
         while (left < right) {
-            char lC = str.charAt(left);
-            char rC = str.charAt(right);
-            if (isDigitalOrLetter(lC) && isDigitalOrLetter(rC)) {
-                if (lC == rC) {
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+            if (Character.isLetterOrDigit(l) && Character.isLetterOrDigit(r)) {
+                if (l == r) {
                     left++;
                     right--;
                 } else {
                     return false;
                 }
-            } else if (!isDigitalOrLetter(lC)) {
+            } else if (!Character.isLetterOrDigit(l)) {
                 left++;
-            } else if (!isDigitalOrLetter(rC)) {
+            } else if (!Character.isLetterOrDigit(r)) {
                 right--;
             }
         }
         return true;
-    }
-
-    private static boolean isDigitalOrLetter(char ch) {
-        return (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z');
-    }
-
-    public static void main(String[] args) {
-        LeetCodeUtil.logln("solution(A man, a plan, a canal: Panama) = " +
-                solution("A man, a plan, a canal: Panama"));
-        LeetCodeUtil.logln("solution(race a car) = " +
-                solution("race a car"));
     }
 }
